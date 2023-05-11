40c20814a332e88e69c46d6cbd92a8793446f997
Joseph <ralphlsulit@gmail.com>
2023-05-11 21:21:42 +0800

    added the left and right side container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 // import { Button } from 'react-bootstrap';
 import './App.css';
+import Rightside from './components/right-side/right-side';
+import Leftside from './components/left-side/left-side';
 
 function App() {
   return (
     <div className="App container">
-      react form test
+      <Leftside />
+      <Rightside />
     </div>
   );
 }
